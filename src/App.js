d0c4f62f3f9fa027a57f0ae079db73a358ca8372
kevir1502 <33071671+kevir1502@users.noreply.github.com>
2021-09-22 16:28:23 -0600

    kevin code

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Search from "./views/Search";
+const App = () => {
+  return <Search />;
+}
+
+export default App;
---
+++ src/views/Search/Components/SearchResults/SearchResultItems.js
@@ -0,0 +1,8 @@
+export default function SearchResultItem({ name, email }) {
+  return (
+    <div style={{ backgroundColor: "#E8E7E7", marginTop: "1rem", marginBottom: "1rem", padding: 10, width: "100%" }}>
+      <p>{name}</p>
+      <p>{email}</p>
+    </div>
+  );
+}
